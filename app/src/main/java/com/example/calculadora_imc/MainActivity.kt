47d49346077f38664ebec35392e43fb32059ba89
5gMurilo <cardoso.murilo2002@gmail.com>
2022-01-19 21:49:30 -0300

    RadioButton is working now

--- app/src/main/java/com/example/calculadora_imc/MainActivity.kt
+++ app/src/main/java/com/example/calculadora_imc/MainActivity.kt
@@ -1,39 +1,59 @@
 package com.example.calculadora_imc
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
-import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.doAfterTextChanged
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private var peso:String? = ""
+    private var altura:String? = ""
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        Log.w("Lifecycle", "entrei no Create - a tela está sendo criada")
+        setListeners()
     }
 
-    override fun onStart() {
-        super.onStart()
-        Log.w("Lifecycle", "entrei no Start - a tela está visível")
+    private fun setListeners() {
+        inptAltura?.doAfterTextChanged {
+            altura = it.toString()
+        }
+        inptPeso?.doAfterTextChanged {
+            peso = it.toString()
+        }
+
+        btnCalcular.setOnClickListener{
+            calcularIMC(peso, altura)
+        }
     }
 
-    override fun onResume() {
-        super.onResume()
-        Log.w("Lifecycle", "entrei no Resume - agora você pode interagir com a tela")
-    }
-
-    override fun onPause() {
-        super.onPause()
-        Log.w("Lifecycle", "entrei no Pause - a tela perdeu o foco, você não consegue interagir")
-    }
-
-    override fun onStop() {
-        super.onStop()
-        Log.w("Lifecycle", "entrei no Stop - a tela não está mais visível, mas ainda existe")
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.w("Lifecycle", "entrei no Destroy - a tela foi destruída")
+    private fun calcularIMC (p:String?, a: String?){
+        if(rbMasc.isChecked ){
+            if (p != null && a != null) {
+                val imc:Float = p.toFloat() / (a.toFloat() * a.toFloat())
+                startActivity(Intent(this, ResultActivity::class.java).apply {
+                    putExtra("imc", imc)
+                    putExtra("sex", "masc")
+                })
+            }else{
+                Toast.makeText(this, "Insira valores válidos nos campos", Toast.LENGTH_SHORT).show()
+            }
+        }else if(rbFem.isChecked){
+            if (p != null && a != null) {
+                val imc:Float = p.toFloat() / (a.toFloat() * a.toFloat())
+                startActivity(Intent(this, ResultActivity::class.java).apply {
+                    putExtra("imc", imc)
+                    putExtra("sex","fem")
+                })
+            }else{
+                Toast.makeText(this, "Insira valores válidos nos campos", Toast.LENGTH_SHORT).show()
+            }
+        }else{
+            Toast.makeText(this, "Selecione seu sexo", Toast.LENGTH_SHORT).show()
+        }
     }
 }
--- app/src/main/java/com/example/calculadora_imc/ResultActivity.kt
+++ app/src/main/java/com/example/calculadora_imc/ResultActivity.kt
@@ -1,14 +1,25 @@
 package com.example.calculadora_imc
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_result.*
 class ResultActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
+
+        setSupportActionBar(toolbar)
+        supportActionBar?.title = ""
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        showIMC()
+
     }
+
+    fun showIMC(){
+        IMC.text = String.format("%.2f", intent.extras?.getFloat("imc"))
+    }
+
+
 
 
 }
