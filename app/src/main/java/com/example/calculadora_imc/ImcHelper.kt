d482e85ad1e6058863a0070d4b843199e7f3d67f
Murilo <cardoso.murilo2002@gmail.com>
2022-01-26 19:00:51 -0300

    :white_check_mark: application passed the tests

--- app/src/main/java/com/example/calculadora_imc/ImcHelper.kt
+++ app/src/main/java/com/example/calculadora_imc/ImcHelper.kt
@@ -1,5 +1,21 @@
 package com.example.calculadora_imc
 
-class ImcHelper() {
+class ImcHelper(private val imcs:List<Imc>) {
+
+    fun getImc(): MutableList<Float> {
+        var lista = mutableListOf<Float>()
+        for(valor in imcs){
+            var calc = valor.peso?.div((valor.altura?.times(valor.altura!!)!!))
+            if (calc != null) {
+                lista.add(calc)
+            }
+        }
+        return lista
+    }
+
+    fun getImcList(): List<Imc>{
+        return imcs
+    }
+
 
 }
--- app/src/test/java/com/example/calculadora_imc/ImcTest.kt
+++ app/src/test/java/com/example/calculadora_imc/ImcTest.kt
@@ -1,20 +1,46 @@
 package com.example.calculadora_imc
 
+import org.junit.Assert
 import org.junit.Test
-
-import org.junit.Assert.*
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
+
+@RunWith(RobolectricTestRunner::class)
 class ImcTest {
+    //mock dos dados - preparação do teste
+    private val imcHelper = ImcHelper(
+        listOf(
+            Imc(
+                93f,
+                1.77f,
+                "masc"
+            ),
+            Imc(
+                104f,
+                1.6f,
+                "fem"
+            )
+        )
+    )
+
     @Test
-    fun addition_isCorrect() {
-        //preparação
-        TODO("fazer os mocks de testes")
+    fun `Quando chamar o metodo getImc com dois imcs deve retornar uma lista com 2 imcs`() {
         //validação
-        TODO("validar os mocks")
+        val list = imcHelper.getImcList()
+        Assert.assertEquals(2, list.size)
+
     }
+
+    @Test
+    fun `Calcular o imc`() {
+        val list = imcHelper.getImc()
+        Assert.assertEquals(40.624996f, list[1])
+    }
+
 }
